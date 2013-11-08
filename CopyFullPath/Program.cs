094d1aaff0bac1a4547d05d9c2f7481bbd12c9ca
Andrew Chaa <andrew.chaa@huddle.com>
2013-11-08 17:37:19 +0000

    initial load

---
+++ CopyFullPath/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CopyFullPath
+{
+    public class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            string fullPath = args[0];
+            System.Windows.Forms.Clipboard.SetText(fullPath);
+        }
+    }
+}
